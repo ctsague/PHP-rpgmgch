026a7b053f6f8be44465b9fd99a269a6b4e3b546
tsague <tsagueymele@yahoo.fr>
2017-01-23 11:45:41 +0100

    Ajout de fichier Style.css et UserTable.php

---
+++ UserTable.php
@@ -0,0 +1,25 @@
+<?php $Users = require('Users.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Users Table</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<table>
+		<tr>
+			<th>ID</th>
+			<th>Email</th>
+			<th>DateCreate</th>
+		</tr>
+		<?php foreach($tabClients as $client) :?>	
+		<tr>
+			<td><?=$client->getId(); ?></td>
+			<td><?=$client->getEmail(); ?></td>
+			<td><?=$client->getCreateAt(); ?></td>
+		</tr>
+		<?php endforeach ?>	
+	</table>	
+</body>
+</html>
